523e05e034be314875654c83ff4cb42f4411475f
SanmithD <sanmithdevadiga7@gmail.com>
2025-07-30 11:18:40 +0530

    minor changes

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ dbConnect();
 
 app.use(
   cors({
-    origin: ['http://localhost:5173'],
+    origin: ['https://offter-letter-client.vercel.app'],
     methods: ["GET", "PUT", "POST", "DELETE", "PATCH"],
     credentials: true,
   })
